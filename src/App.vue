2be1b7a6c75f27793631339307d3a4ac43a42109
Yahya Hosseini <yahyahosseini1989@gmail.com>
2021-09-29 23:23:18 +0330

    Feature: app installation and basic dependencies

---
+++ src/App.vue
@@ -0,0 +1,72 @@
+<template>
+  <v-app id="app">
+    <!-- <div id="nav">
+      <router-link to="/">Home</router-link> |
+      <router-link to="/about">About</router-link>
+    </div>
+    <router-view/> -->
+    <v-navigation-drawer
+      v-model="drawer"
+      app
+      right
+      class="pt-4"
+      color="blue lighten-0"
+      mini-variant
+    >
+      <router-link to="/">
+        <v-icon
+          large
+          color="blue darken-2"
+        >
+          mdi-domain
+        </v-icon>
+      </router-link>
+      <router-link to="/UNewProduct">
+        <v-icon
+          large
+          color="blue darken-2"
+        >
+          mdi-domain
+        </v-icon>
+      </router-link>
+
+    </v-navigation-drawer>
+
+    <v-main class="grey lighten-3">
+      <router-view />
+    </v-main>
+  </v-app>
+</template>
+
+<script>
+export default {
+  name: "App",
+
+  data: () => ({
+    //
+  }),
+};
+</script>
+
+<style lang="scss">
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+}
+
+#nav {
+  padding: 30px;
+
+  a {
+    font-weight: bold;
+    color: #2c3e50;
+
+    &.router-link-exact-active {
+      color: #42b983;
+    }
+  }
+}
+</style>
---
+++ src/views/UPanel/UProducts/UNewProducts/UNewProduct.vue
@@ -0,0 +1,15 @@
+<template>
+  <div>
+    <add-simple-product />
+  </div>
+</template>
+
+<script>
+import AddSimpleProduct from "./partials/AddSimpleProduct.vue";
+export default {
+  components: { AddSimpleProduct },
+};
+</script>
+
+<style lang="scss" scoped>
+</style>

ea4ba38cecece3de3d25721702ec1501500baa06
Yahya Hosseini <HosseiniY@safarayaneh.local>
2021-10-06 17:15:12 +0330

    add uploader to project

---
+++ src/components/ByUploader.vue
@@ -0,0 +1,75 @@
+<!-- https://www.npmjs.com/package/vue-upload-drop-images -->
+
+<template>
+  <div class="byUploader">
+    <UploadImages
+      uploadMsg="عکس را اینجا رها کن"
+      :max="1"
+      maxError=".فقط یک تصویر برای آپلود مجاز میباشد"
+      @changed="handleImages"
+    />
+    <!-- <UploadImages
+      :max="max"
+      :uploadMsg="uploadMsg"
+      :maxError="maxError"
+      :fileError="fileError"
+      :clearAll="clearAll"
+    /> -->
+  </div>
+</template>
+
+<script>
+import UploadImages from "vue-upload-drop-images";
+
+export default {
+  name: "ByUploader",
+  components: {
+    UploadImages,
+  },
+  props: {
+    max: {
+      type: Number,
+      default: 1,
+    },
+    uploadMsg: {
+      type: String,
+    },
+    maxError: {
+      type: String,
+    },
+    fileError: {
+      type: String,
+    },
+    clearAll: {
+      type: String,
+    },
+  },
+  data() {
+    return {
+      hasImage: false,
+      image: null,
+    };
+  },
+  methods: {
+    handleImages(files) {
+      console.log(files);
+      this.$emit("files", files);
+      /*
+        [
+        {
+            "name": "Screenshot from 2021-02-23 12-36-33.png",
+            "size": 319775,
+            "type": "image/png",
+            "lastModified": 1614080193596
+            ...
+        },
+        ...
+        ]
+        */
+    },
+  },
+};
+</script>
+
+<style>
+</style>
---
+++ src/components/common/ByCheckbox.vue
@@ -0,0 +1,46 @@
+<template>
+  <div :class="className">
+    <v-checkbox
+      class="byCheckbox"
+      :value="value"
+      @input="updateText"
+      :label="label"
+      :rules="rules"
+      :required="required"
+      :color="color"
+    >
+    </v-checkbox>
+  </div>
+</template>
+
+<script>
+export default {
+  inheritAttrs: false,
+  name: "ByCheckbox",
+  props: {
+    className: { type: String, default: "" },
+    value: { type: Boolean, default: false },
+    required: { type: Boolean, default: false },
+    label: { type: String, default: "" },
+    color: { type: String, default: "#2563eb" },
+    loading: { type: Boolean, default: false },
+  },
+  methods: {
+    updateText($value) {
+      this.$emit("input", $value);
+    },
+  },
+  computed: {
+    rules() {
+      if (this.required) {
+        return [(v) => !!v || "این فیلد ضروری میباشد."];
+      } else {
+        return [];
+      }
+    },
+  },
+};
+</script>
+
+<style lang="scss" scoped>
+</style>
